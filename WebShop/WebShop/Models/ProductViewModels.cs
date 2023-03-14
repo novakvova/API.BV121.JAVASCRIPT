cd8780bcdf4133ec298f376fdc0b733a5342d6b2
novakvova <novakvova@gmail>
2023-03-14 19:30:52 +0200

    Add and show product working is good

---
+++ WebShop/WebShop/Models/ProductViewModels.cs
@@ -0,0 +1,21 @@
+﻿namespace WebShop.Models
+{
+    public class ProductItemView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public List<string> Images { get; set; }
+    }
+
+    public class ProductCreateView
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public List<IFormFile> Files { get; set; }
+    }
+}
